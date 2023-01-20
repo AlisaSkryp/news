fe108aa6c83bb43c94077bc800e0700034eb620f
AlisaSkryp <topt18@gmail.com>
2023-01-20 17:50:45 +0100

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,1 +1,18 @@
-<template></template>
+<template>
+  <header class="header">
+    <div class="header__nav">
+      <ul class="header__list">
+        <li>fgdfg</li>
+        <li>fgdfg</li>
+        <li>gfdg</li>
+      </ul>
+    </div>
+  </header>
+</template>
+<style>
+.header {
+  display: flex;
+  width: 100%;
+  height: 80px;
+}
+</style>
